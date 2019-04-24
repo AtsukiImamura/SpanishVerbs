303fb1a19e63b89468695962457efa9cfa806692
AtsukiImamura <ohmoon.au@gmail.com>
2019-04-24 14:51:08 +0900

    'sass使えるように'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const VueLoaderplugin = require('vue-loader/lib/plugin');//vue-loader/lib/plugin
 const path = require('path')
+const enabledSourceMap = true
 
 module.exports = {
     entry: './src/index.js',
@@ -26,6 +27,35 @@ module.exports = {
                 use: [
                     'vue-style-loader',
                     'css-loader'
+                ]
+            },
+            {
+                test: /\.scss/, // 対象となるファイルの拡張子
+                use: [
+                    // linkタグに出力する機能
+                    "style-loader",
+                    // CSSをバンドルするための機能
+                    {
+                        loader: "css-loader",
+                        options: {
+                        // オプションでCSS内のurl()メソッドの取り込みを禁止する
+                        url: false,
+                        // ソースマップの利用有無
+                        sourceMap: enabledSourceMap,
+            
+                        // 0 => no loaders (default);
+                        // 1 => postcss-loader;
+                        // 2 => postcss-loader, sass-loader
+                        importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                        // ソースマップの利用有無
+                        sourceMap: enabledSourceMap
+                        }
+                    }
                 ]
             }
         ]
